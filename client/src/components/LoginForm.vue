d4d738790bdf1bb3d23a322e26b494005efc38fb
Brian Gottfried <bgottf@gmail.com>
2018-09-07 00:27:42 -0400

    Create login, create account components
    No javascript logic yet.

--- client/src/components/LoginForm.vue
+++ client/src/components/LoginForm.vue
@@ -1,17 +1,14 @@
 <template lang='pug'>
-  v-container(fluid fill-height)
-    v-layout(align-center justify-center)
-      v-flex(xs12 sm8 md4)
-        v-card.elevation-12
-          v-toolbar(dark color='primary')
-            v-toolbar-title Login form
-            v-card-text
-              v-form
-                v-text-field(prepend-icon="person" name="login" label="Login" type="text")
-                v-text-field(id="password" prepend-icon="lock" name="password" label="Password" type="password")
-            v-card-actions
-              v-spacer
-              v-btn(color='primary') Login
+  v-card
+    //- v-toolbar(dense flat dark color='primary')
+    //-   v-toolbar-header LOGIN
+    v-card-text
+      v-form
+        v-text-field(prepend-icon="person" name="email" label="E-mail" type="text")
+        v-text-field(id="password" prepend-icon="lock" name="password" label="Password" type="password")
+    v-card-actions
+      v-spacer
+      v-btn(color='primary') Log In
 </template>
 
 <script>
@@ -21,4 +18,3 @@ export default {
 
 <style>
 </style>
-
