cabdb6f82125c0b20f8bbf043870b10362be54ce
Brian Gottfried <bgottf@gmail.com>
2018-09-09 19:32:55 -0400

    update theme colors

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -5,8 +5,8 @@ import colors from 'vuetify/es5/util/colors';
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#363640',
-    secondary: colors.grey.lighten4,
+    primary: '#282F3C',
+    secondary: '#ECEFF1',
     accent: colors.lime.lighten3,
   },
 });
