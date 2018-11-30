524b0b58e7d8766b4a8bd8e4cd8c6724eaad14c5
Brian Gottfried <bgottf@gmail.com>
2018-11-30 16:04:33 -0500

    Add variable table component
    This is for variable descriptions inside dataset view.

---
+++ client/src/components/DatasetManager/VariableTable.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-data-table
+    :headers='headers'
+    :items='variables'
+  >
+    <template
+      slot='items'
+      slot-scope='props'
+    >
+      <tr>
+        <td> {{ props.item.name }} </td>
+        <td> {{ props.item.type }} </td>
+        <td> {{ props.item.description }} </td>
+      </tr>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+  export default {
+    props: {
+      variables: Array,
+    },
+    data() {
+      return {
+        headers: [
+          {
+            text: 'Variable',
+            value: 'variable',
+          },
+          {
+            text: 'Type',
+            value: 'type',
+          },
+          {
+            text: 'Description',
+            value: 'description',
+          },
+        ],
+      }
+    },
+  }
+</script>
+
+<style scoped>
+</style>
