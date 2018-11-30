00ded70cc78590bd0d34b0ca53cf115682a3d2f7
Brian Gottfried <bgottf@gmail.com>
2018-11-30 13:36:05 -0500

    Add Dataset view for displaying dataset information

---
+++ client/src/views/Dataset.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container
+    fluid
+    fill-height
+    grid-list-xl
+  >
+    <v-toolbar
+      app
+      class="primary"
+    >
+      <v-toolbar-items>
+        <v-btn
+          flat
+          dark
+          @click='goBack'
+        >
+          <v-icon left>
+            arrow_back
+          </v-icon>
+          BACK TO DATASET MANAGER
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-container>
+
+</template>
+
+<script>
+  export default {
+    props: {
+      id: String,
+    },
+    methods: {
+      goBack() {
+        this.$router.go(-1);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
