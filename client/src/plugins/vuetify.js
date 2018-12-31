8e39f6a3f052da2abb32acc6787fd71417212d3a
Brian Gottfried <bgottf@gmail.com>
2018-12-30 21:00:24 -0500

    Update theme colors

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -6,9 +6,12 @@ import 'vuetify/dist/vuetify.min.css';
 Vue.use(Vuetify, {
   theme: {
     primary: '#212B3A',
+    // primary: '#2c3e50',
+    // primary: '#013852',
     // primary: '#2C384A',
     secondary: '#038ccf',
     accent: '#282F3C',
     bgColor: '#f5f7f7',
+    iconColor: '#99a4ae',
   },
 });
