c33328dd2602051e707599e15a89633bd8c16156
Brian Gottfried <bgottf@gmail.com>
2019-01-16 15:55:49 -0500

    Include new cohort module to store

--- client/src/store/modules/index.js
+++ client/src/store/modules/index.js
@@ -1,10 +1,14 @@
 import auth from './auth';
+import cohort from './cohort';
+import dashboard from './dashboard';
 import datasetManager from './datasetManager';
 import cohortManager from './cohortManager';
 // import other stores
 
 export default {
   auth,
+  cohort,
+  dashboard,
   datasetManager,
   cohortManager,
 };
