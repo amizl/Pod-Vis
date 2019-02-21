647459f29612e1386591b4f7ef55e94104cba796
Brian Gottfried <bgottf@gmail.com>
2019-02-20 22:05:06 -0500

    Allow Vue dev server to proxy to flask API
    We do this to keep us from having to rebuild the docker image if we are just focusing development on the client. We can spin up the docker containers with flask and databases and leave, run the node dev server and get hot reloading during development.

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,16 @@
 const webpack = require('webpack');
 
 module.exports = {
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:5000',
+      },
+      '^/auth': {
+        target: 'http://localhost:5000',
+      },
+    },
+  },
   configureWebpack: {
     plugins: [
       new webpack.LoaderOptionsPlugin({
