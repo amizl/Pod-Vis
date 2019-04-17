f9f8acd37f217553ee382879dce2db727f850fa9
Brian Gottfried <bgottf@gmail.com>
2019-04-17 11:44:32 -0400

    Add new models to init

--- api/app/models/__init__.py
+++ api/app/models/__init__.py
@@ -11,6 +11,7 @@ from .observation import Observation
 from .study import Study
 from .project import Project
 from .collection import Collection
+from .collection_observation_variable import CollectionObservationVariable
 from .collection_query import CollectionQuery
 from .collection_study import CollectionStudy
 from .subject_attribute import SubjectAttribute
