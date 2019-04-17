ea273be2cc2211383c141cb54d6e92ac670f045c
Brian Gottfried <bgottf@gmail.com>
2019-04-17 13:31:08 -0400

    Fix circular import errors

--- api/app/auth/__init__.py
+++ api/app/auth/__init__.py
@@ -3,10 +3,12 @@ from flask_jwt_extended import JWTManager
 
 jwt = JWTManager()
 
-from .utils import *
 
 # Create and register auth blueprint. This blue print
 # is responsible for authentication routes under /auth/
 auth = Blueprint("auth", __name__)
+
+# Import utils down here to avoid circular imports of auth
+from .utils import create_tokens, custom_user_loader_error, user_loader_callback
 # Import routes in order to register auth endpoints
 import app.auth.routes
