85e111ce78a2cd8b4e2c68d8b789cd61e76d87a7
Brian Gottfried <bgottf@gmail.com>
2019-04-22 01:04:52 -0400

    Begin create cohort page

--- client/src/views/BuildCohort.vue
+++ client/src/views/BuildCohort.vue
@@ -1,105 +1,33 @@
 <template>
   <v-container fluid>
-    <v-toolbar class="background" flat app>
+    <v-toolbar app class="white">
+      <v-toolbar-title>Create Cohort</v-toolbar-title>
       <v-spacer></v-spacer>
-      <!-- <v-btn flat color="primary">
-        <v-icon left="">save</v-icon>
-        Save Cohort
-      </v-btn> -->
     </v-toolbar>
-    <v-layout justify-center>
-      <v-flex xs10>
-        <p class="subheading foo--text mt-3">
-          You are building a cohort from
-          <span class="font-weight-bold"> {{ name }}</span>
-        </p>
-        <v-divider></v-divider>
-      </v-flex>
-    </v-layout>
-    <!-- <v-layout class="pt-2" justify-center>
-      <v-flex xs10>
-        <v-select
-          :items="[{ name: 'Gender' }]"
-          v-model="e11"
-          chips
-          label="Select Dimensions"
-          item-text="name"
-          item-value="name"
-          max-height="auto"
-          autocomplete
-          multiple
-          solo
-          flat
-        >
-        </v-select>
-        <v-btn flat>
-          <v-icon left>format_color_fill</v-icon>
-          Color</v-btn
-        >
-        <v-switch v-model="hideTicks" label="HIDE TICKS"></v-switch>
-      </v-flex>
-    </v-layout> -->
-    <v-layout class="pt-5" justify-space-around wrap>
-      <!-- <v-progress-circular
-        v-if="loading"
-        indeterminate
-        color="secondary"
-        size="100"
-        width="10"
-      ></v-progress-circular> -->
-      <v-flex xs10>
-        <v-card class="ui-card">
-          <parallel-coordinates :ticks="!hideTicks"></parallel-coordinates>
-        </v-card>
-      </v-flex>
+    <v-layout align-center justify-center>
+      <h1 class="headline text-md-center">
+        <v-icon large class="pb-4">build</v-icon><br />
+        Creating a cohort is currently under development
+      </h1>
     </v-layout>
   </v-container>
 </template>
+
 <script>
-import { mapState, mapGetters, mapActions } from 'vuex';
-import { actions, getters, state } from '@/store/modules/cohort/types';
-import CohortCard from '@/components/CohortManager/CohortCard.vue';
-import ParallelCoordinates from '@/components/charts/ParallelCoordinates.vue';
-import ContentHeader from '../components/layout/Header.vue';
+// import { mapState, mapActions } from 'vuex';
+// import { state, actions } from '@/store/modules/cohortManager/types';
 
 export default {
-  components: {
-    CohortCard,
-    ContentHeader,
-    ParallelCoordinates,
-  },
   props: {
+    // id is passed in via route parameters
+    // i.e., /build?id=x&id=y
     id: {
-      type: String,
-      required: true,
+      type: [String, Array],
+      default: null,
     },
   },
-  data() {
-    return {
-      hideTicks: false,
-      e11: [],
-    };
-  },
-  computed: {
-    ...mapGetters('cohort', {
-      name: getters.GET_DATASET_NAME,
-    }),
-    // loading() {
-    //   return Object.keys(this.dataset) == 0;
-    // },
-  },
-  async created() {
-    await this.fetchDataset(this.id);
-    // const dataset = await this.fetchDataset(this.id);
-    // if (dataset.exists) {
-    //   this.dataset = dataset.data();
-    // }
-  },
-  methods: {
-    ...mapActions('cohort', {
-      fetchDataset: actions.FETCH_DATASET,
-    }),
-  },
+  computed: {},
+  methods: {},
 };
 </script>
 
