e1ab7feb7aa95512f7ef28ddb7a26b303572200c
Brian Gottfried <bgottf@gmail.com>
2019-04-26 13:10:39 -0400

    Remove firebase dependency

--- ui/src/store/modules/cohort/actions.js
+++ ui/src/store/modules/cohort/actions.js
@@ -1,33 +1,11 @@
-import * as firebase from 'firebase';
 // import store from '@/store';
 import { actions, mutations } from './types';
 
 export default {
   async [actions.FETCH_DATASET]({ commit }, payload) {
-    commit(mutations.SET_LOADING, true);
-    firebase
-      .firestore()
-      .collection('datasets')
-      .doc(payload)
-      .get()
-      .then(querySnapshot => {
-        const dataset = querySnapshot.data();
-        commit(mutations.SET_DATASET, dataset);
-        commit(mutations.SET_LOADING, false);
-      });
+    // commit(mutations.SET_LOADING, true);
   },
   [actions.SET_COHORT_SELECTION]({ commit }, payload) {
     commit(mutations.SET_COHORT_SELECTION, payload);
   },
-  // firebase
-  //   .firestore()
-  //   .collection('users')
-  //   .doc(store.state.auth.user)
-  //   .collection('cohorts')
-  //   .get()
-  //   .then(querySnapshot => {
-  //     const cohorts = querySnapshot.docs.map(doc => doc.data());
-  //     commit(mutations.SET_COHORTS, cohorts);
-  //     commit(mutations.SET_LOADING, false);
-  //   });
 };
