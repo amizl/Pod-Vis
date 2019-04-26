1d340a80af7cc2e999f4934d549b71f003411529
Brian Gottfried <bgottf@gmail.com>
2019-04-26 09:47:59 -0400

    Fix auth guard authenticating
    This may need to be revisited. If the server is busy with requests, when a user enters a page it stalls until server can finish authentication request.

--- client/src/router/authGuards.js
+++ client/src/router/authGuards.js
@@ -18,23 +18,28 @@ const authModule = 'auth';
  * Lazy evaluated function to determine if user is authenticated.
  */
 const isUserAuthenticated = () => {
-  const isUserAuthenticated = authGetters.IS_USER_AUTHENTICATED;
-  return store.getters[`${authModule}/${isUserAuthenticated}`];
+  return store.getters[`${authModule}/${authGetters.IS_USER_AUTHENTICATED}`];
 };
 
 export const requireAuth = async (to, from, next) => {
-  await store.dispatch(`${authModule}/${authActions.VALIDATE_TOKEN}`);
-  if (!isUserAuthenticated()) next('/signin');
-  next();
+  try {
+    await store.dispatch(`${authModule}/${authActions.VALIDATE_TOKEN}`);
+    next();
+  } catch (err) {
+    next('/signin');
+  }
 };
 
 export const requireNotAuth = async (to, from, next) => {
-  // await store.dispatch(`${authModule}/${authActions.VALIDATE_TOKEN}`);
   // If user was routed to signin somehow from a route,
   // and they are still authenticated, keep them on their
   // current route.
-  if (isUserAuthenticated() && from.name) next(from.name);
   // Otherwise route them to the dashboard.
-  if (isUserAuthenticated() && !from.name) next('/dashboard');
-  next();
+  try {
+    await store.dispatch(`${authModule}/${authActions.VALIDATE_TOKEN}`);
+    if (isUserAuthenticated() && from.name) next(from.name);
+    if (isUserAuthenticated() && !from.name) next('/dashboard');
+  } catch (err) {
+    next();
+  }
 };
