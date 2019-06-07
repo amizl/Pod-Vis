d1e54978de0e8b04f32c0160ceea9088ccddb3ac
Brian Gottfried <bgottf@gmail.com>
2019-06-07 10:19:46 -0400

    Normalize by duration between first and last visit

--- api/app/models/collection.py
+++ api/app/models/collection.py
@@ -157,8 +157,23 @@ class Collection(db.Model):
         ]).set_index('id')
         # Convert to datetime
         if "Birthdate" in subjects_df.columns:
+            query_for_first_visit = text("""
+                SELECT subject_id, min(event_date) as first_visit
+                FROM subject_visit
+                JOIN subject on subject.id = subject_visit.subject_id
+                GROUP BY subject_id;
+            """)
+            result_proxy = connection.execute(query_for_first_visit)
+            result = [dict(row) for row in result_proxy]
+            first_visit_df = pd.DataFrame(result)
+            subjects_df = pd.merge(subjects_df, first_visit_df, left_on='id', right_on='subject_id')
             subjects_df['Birthdate'] = pd.to_datetime(subjects_df['Birthdate'])
+            subjects_df['first_visit'] = pd.to_datetime(subjects_df['first_visit'])
+            # subjects_df['age'] = subjects_df['first_visit'] - subjects_df['Birthdate'].year
+            # return subjects_df
+
         observation_variable_ids = [obs_var.ontology.id for obs_var in self.observation_variables]
+
         subject_variable_ids = [subject_var.id for subject_var in self.subject_variables]
 
         # Query for getting observation totals for patients across all
@@ -188,6 +203,11 @@ class Collection(db.Model):
             .fetchall()
         result = [dict(row) for row in result_proxy]
         result_df = pd.DataFrame(result)
+
+        # first_events = dict()
+        # for subject_id, first_event in result_proxy:
+        #     first_events[subject_id] = first_event
+
         # Compute ROC for each patient and each scale from their first and last visit
         observations = []
         for (subject_id, obs_id), df in result_df.groupby(['subject_id', 'observation_ontology_id']):
@@ -199,7 +219,8 @@ class Collection(db.Model):
             n = len(totals)
             M = totals.diff(n-1)
             N = totals.shift(n-1)
-            roc = (M / N) * 100
+            # normalize by both minimum visit and duration
+            roc = ((M / N) * 100) / ((last_date.year - first_date.year)) # / 365.25)
 
             observations.append(
                 dict(
@@ -210,7 +231,7 @@ class Collection(db.Model):
                     max=totals.loc[last_date]
                 ))
 
-        return pd.merge(pd.DataFrame(observations), subjects_df, left_on='subject_id', right_on='id')
+        return pd.merge(pd.DataFrame(observations), subjects_df, left_on='subject_id', right_on='subject_id')
 
     def save_to_db(self):
         """Save collection to the database."""
