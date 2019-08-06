cb3e2301880194aab02984b2ded286ed45cb3520
Brian Gottfried <bgottf@gmail.com>
2019-08-06 16:12:27 -0400

    Outcome variables component setup in data explorer
    Progress #61

--- ui/src/components/DataExplorer/OutcomeVariables.vue
+++ ui/src/components/DataExplorer/OutcomeVariables.vue
@@ -2,11 +2,39 @@
   <v-sheet color="white" height="100%" class="rounded-lg shadow">
     <v-card-title class="title primary--text"> Outcome Variables </v-card-title>
     <v-divider></v-divider>
+    <v-card-text>
+      <v-treeview
+        v-model="selectedObservationVariables"
+        color="primary"
+        selectable
+        return-object
+        :items="observationVariables"
+        item-text="label"
+      ></v-treeview>
+    </v-card-text>
   </v-sheet>
 </template>
 
 <script>
-export default {};
+import { mapState } from 'vuex';
+import { state } from '@/store/modules/dataExplorer/types';
+
+export default {
+  data() {
+    return {
+      selectedObservationVariables: [],
+      observationVariables: [],
+    };
+  },
+  computed: {
+    ...mapState('dataExplorer', {
+      collection: state.COLLECTION,
+    }),
+  },
+  async created() {
+    this.observationVariables = this.collection.observation_variables;
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
