72f3ffaf6b8fdbff9650566b9efe325e15e03cc8
Brian Gottfried <bgottf@gmail.com>
2019-08-06 14:21:43 -0400

    Setup 4-panel layout for data explorer

--- ui/src/views/DataExplorer.vue
+++ ui/src/views/DataExplorer.vue
@@ -1,26 +1,49 @@
 <template>
-  <v-container fluid>
-    <v-toolbar app class="white">
-      <v-toolbar-title>Data Explorer</v-toolbar-title>
+  <v-container v-if="isLoading" fluid fill-height>
+    <loading-spinner />
+  </v-container>
+  <v-container v-else fluid fill-height grid-list-md>
+    <v-toolbar app class="primary">
+      <v-toolbar-title class="white--text">Data Explorer</v-toolbar-title>
     </v-toolbar>
-    <v-layout row wrap>
-      <v-flex xs12>
-        <v-card color="blue-grey darken-2" class="white--text">
-          DatasetSummaryCard here
-          <!-- Import DataExplorer/DatasetSummaryCard.vue -->
-        </v-card>
+    <v-layout column fill-height>
+      <v-flex>
+        <v-layout fill-height>
+          <v-flex xs3>
+            <v-sheet color="white" height="100%" class="rounded-lg shadow">
+              <v-card-title class="title primary--text">
+                Outcome Variables
+              </v-card-title>
+              <v-divider></v-divider>
+            </v-sheet>
+          </v-flex>
+          <v-flex xs9>
+            <v-sheet color="white" height="100%" class="rounded-lg shadow">
+              <v-card-title class="title primary--text"
+                >Summary View</v-card-title
+              >
+              <v-divider></v-divider>
+            </v-sheet>
+          </v-flex>
+        </v-layout>
       </v-flex>
-      <v-flex xs12>
-        <v-card color="green darken-2" class="white--text">
-          <SummaryViewCard></SummaryViewCard>
-          <!-- Import DataExplorer/SummaryViewCard.vue -->
-        </v-card>
-      </v-flex>
-      <v-flex xs12>
-        <v-card color="blue darken-2" class="white--text">
-          DetailedViewCard here
-          <!-- Import DataExplorer/DetailedViewCard.vue -->
-        </v-card>
+      <v-flex>
+        <v-layout fill-height>
+          <v-flex xs3>
+            <v-sheet color="white" height="100%" class="rounded-lg shadow">
+              <v-card-title class="title primary--text">Cohorts</v-card-title>
+              <v-divider></v-divider>
+            </v-sheet>
+          </v-flex>
+          <v-flex>
+            <v-sheet color="white" height="100%" class="rounded-lg shadow">
+              <v-card-title class="title primary--text"
+                >Detailed View</v-card-title
+              >
+              <v-divider></v-divider>
+            </v-sheet>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -32,9 +55,17 @@ import DetailedViewCard from '@/components/DataExplorer/DetailedViewCard.vue';
 import SummaryViewCard from '@/components/DataExplorer/SummaryViewCard.vue';
 
 export default {
-    components: {
-        SummaryViewCard, DetailedViewCard, DatasetSummaryCard
-    }
+  components: {
+    SummaryViewCard,
+    DetailedViewCard,
+    DatasetSummaryCard,
+  },
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  created() {},
 };
 </script>
 
