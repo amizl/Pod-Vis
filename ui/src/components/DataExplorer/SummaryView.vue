b23515e7bfd86854cc1095093a3e8264389035e4
Brian Gottfried <bgottf@gmail.com>
2019-08-08 11:44:32 -0400

    Summary view components progress

--- ui/src/components/DataExplorer/SummaryView.vue
+++ ui/src/components/DataExplorer/SummaryView.vue
@@ -1,12 +1,42 @@
 <template>
   <v-sheet color="white" height="100%" class="rounded-lg shadow">
-    <v-card-title class="title primary--text">Summary View</v-card-title>
-    <v-divider></v-divider>
+    <v-layout column fill-height class="ma-1">
+      <summary-view-toolbar></summary-view-toolbar>
+      <v-container fluid fill-height class="pa-0 pl-3">
+        <summary-parallel-coordinates v-if="userAddedOutcomeVariables" />
+        <v-layout v-else column align-center justify-center fill-height>
+          <v-subheader class="display-1 primary--text text--lighten-5">
+            ADD OUTCOME VARIABLES
+          </v-subheader>
+        </v-layout>
+      </v-container>
+    </v-layout>
   </v-sheet>
 </template>
 
 <script>
-export default {};
+import { mapState } from 'vuex';
+import { state } from '@/store/modules/dataExplorer/types';
+import SummaryParallelCoordinates from '@/components/DataExplorer/SummaryParallelCoordinates.vue';
+import SummaryViewToolbar from '@/components/DataExplorer/SummaryViewToolbar.vue';
+
+export default {
+  components: {
+    SummaryParallelCoordinates,
+    SummaryViewToolbar,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState('dataExplorer', {
+      outcomeVariables: state.OUTCOME_VARIABLES,
+    }),
+    userAddedOutcomeVariables() {
+      return this.outcomeVariables.length > 0;
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
