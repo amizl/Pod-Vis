6510207df3bec04557bc61d523c37f34b743312f
Brian Gottfried <bgottf@gmail.com>
2019-08-08 11:46:10 -0400

    Initial detailed view component for data explorer

--- ui/src/components/DataExplorer/DetailedView.vue
+++ ui/src/components/DataExplorer/DetailedView.vue
@@ -1,7 +1,16 @@
 <template>
   <v-sheet color="white" height="100%" class="rounded-lg shadow">
-    <v-card-title class="title primary--text">Detailed View</v-card-title>
-    <v-divider></v-divider>
+    <v-layout column fill-height class="ma-1">
+      <v-card-title class="title primary--text">Detailed View</v-card-title>
+      <v-divider></v-divider>
+      <v-container fluid fill-height>
+        <v-layout column align-center justify-center fill-height>
+          <v-subheader class="display-1 primary--text text--lighten-5">
+            SELECT OUTCOME VARIABLE
+          </v-subheader>
+        </v-layout>
+      </v-container>
+    </v-layout>
   </v-sheet>
 </template>
 
