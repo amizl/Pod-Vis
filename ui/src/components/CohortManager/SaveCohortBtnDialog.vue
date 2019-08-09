270a65a7805019457de45b8d0f2f580d0ba29273
Brian Gottfried <bgottf@gmail.com>
2019-08-09 17:01:42 -0400

    Save cohort component progress

--- ui/src/components/CohortManager/SaveCohortBtnDialog.vue
+++ ui/src/components/CohortManager/SaveCohortBtnDialog.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-btn
-      :disabled="!areVariablesSelected"
+      :disabled="!hasUserFilteredInputVariables"
       color="primary--text"
       @click="dialog = !dialog"
     >
@@ -9,10 +9,11 @@
     </v-btn>
     <!-- SAVE COLLECTION FORM DIALOG -->
     <v-dialog v-model="dialog" width="500">
-      <v-card>
-        <v-card-title primary-title>
-          <span class="title pl-2">Save Cohort</span>
+      <!-- <v-card max-height="500px" class="rounded-lg">
+        <v-card-title class="title primary--text text--darken-3">
+          Save Cohort
         </v-card-title>
+        <v-sheet class="pa-3 background"> </v-sheet>
         <v-card-text>
           <v-form ref="form" v-model="valid" @submit.prevent="onSaveCollection">
             <v-text-field
@@ -21,7 +22,7 @@
                 () => !!collectionName || 'Collection name is required.',
               ]"
               prepend-inner-icon="table_chart"
-              label="Please name your dataset."
+              label="Please name your cohort."
               box
               flat
               background-color="grey lighten-4"
@@ -34,13 +35,42 @@
         <v-divider></v-divider>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn @click="dialog = false">Cancel</v-btn>
-          <v-btn
-            :loading="loading"
-            color="primary darken-4"
-            @click="onSaveCollection"
+          <v-btn color="primary" flat @click="dialog = false"
+            ><v-icon left dark>close</v-icon>Close</v-btn
           >
-            <v-icon left>save</v-icon> Save</v-btn
+        </v-card-actions>
+      </v-card> -->
+      <v-card class="rounded-lg">
+        <v-card-title primary-title>
+          <span class="primary--text title pl-2">Save Cohort</span>
+        </v-card-title>
+        <v-sheet class="pa-3 background"> </v-sheet>
+        <v-card-text>
+          <v-form ref="form" v-model="valid" @submit.prevent="onSaveCollection">
+            <v-text-field
+              v-model="collectionName"
+              :rules="[
+                () => !!collectionName || 'Collection name is required.',
+              ]"
+              prepend-inner-icon="table_chart"
+              label="Please name your cohort."
+              box
+              flat
+              background-color="grey lighten-4"
+              class="mt-2"
+              hide-details
+            >
+            </v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn flat color="red lighten-2" @click="dialog = false">
+            <v-icon left>close</v-icon> Cancel
+          </v-btn>
+          <v-btn :loading="loading" color="primary" @click="onSaveCollection">
+            <v-icon left>save</v-icon> Save Cohort</v-btn
           >
         </v-card-actions>
       </v-card>
@@ -49,8 +79,8 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex';
-import { actions } from '@/store/modules/datasetManager/types';
+import { mapActions, mapGetters } from 'vuex';
+import { actions, getters } from '@/store/modules/cohortManager/types';
 
 // which store?
 export default {
@@ -71,6 +101,9 @@ export default {
     loading: false,
   }),
   computed: {
+    ...mapGetters('cohortManager', {
+      hasUserFilteredInputVariables: getters.HAS_USER_FILTERED_INPUT_VARIABLES,
+    }),
     areVariablesSelected() {
       return this.variables.length > 0;
     },
