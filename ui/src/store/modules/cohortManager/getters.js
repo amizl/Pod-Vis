fbfdae1e64927f7e7e99ccdfb4ac870ed42a6f02
Brian Gottfried <bgottf@gmail.com>
2019-08-15 16:00:32 -0400

    Getter functions to easier grab cohort data

--- ui/src/store/modules/cohortManager/getters.js
+++ ui/src/store/modules/cohortManager/getters.js
@@ -1,4 +1,5 @@
 import { getters, state as stateTypes } from './types';
+import { nest } from 'd3-collection';
 
 export default {
   [getters.HAS_USER_FILTERED_INPUT_VARIABLES]: state => {
@@ -9,5 +10,158 @@ export default {
       Object.entries(queries).length > NO_QUERIES &&
       queries.constructor === Object
     );
+  },
+  [getters.HAS_USER_SELECTED_COHORT]: state => {
+    const cohort = state[stateTypes.COHORT];
+    if (cohort.id == null) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  [getters.FIND_COHORT_QUERY]: state => {
+    return dimensionName => {
+      return state.cohort.queries.filter(query => {
+        const inputVariable = query.input_variable;
+        // Need to safely access correct attribute
+        if (inputVariable.study) {
+          return inputVariable.study.label === dimensionName;
+        } else if (inputVariable.observation_ontology) {
+          // TODO: This needs to be refacotred. Dimensions are currently
+          // keyed strangely and needs to be better named for an easier
+          // mapping then having to format labels like this...
+          const observationLabel = inputVariable.observation_ontology.label;
+          const dimLabel = inputVariable.dimension_label;
+          let newLabel = '';
+          if (dimLabel === 'change') {
+            newLabel = `${observationLabel} - Change`;
+          } else if (dimLabel === 'roc') {
+            newLabel = `${observationLabel} - Rate of Change`;
+          } else if (dimLabel === 'left_y_axis') {
+            newLabel = `${observationLabel} - First Visit`;
+          } else if (dimLabel === 'right_y_axis') {
+            newLabel = `${observationLabel} - Last Visit`;
+          }
+          return newLabel === dimensionName;
+        } else {
+          return inputVariable.subject_ontology.label === dimensionName;
+        }
+      });
+    };
+  },
+  [getters.FIND_COHORT_STUDY_INPUT_VARIABLES]: state => {
+    return !state.cohort.input_variables
+      ? []
+      : state.cohort.input_variables
+          .filter(variable => variable.study != null)
+          .map(variable => ({ ...variable.study, type: 'study' }));
+  },
+  [getters.FIND_COHORT_SUBJECT_INPUT_VARIABLES]: state => {
+    return !state.cohort.input_variables
+      ? []
+      : state.cohort.input_variables
+          .filter(variable => variable.subject_ontology != null)
+          .map(variable => ({ ...variable.subject_ontology, type: 'subject' }));
+  },
+  [getters.FIND_COHORT_OBSERVATION_INPUT_VARIABLES]: state => {
+    // return !state.cohort.input_variables
+    //   ? []
+    //   : state.cohort.input_variables
+    //       .filter(variable => variable.observation_ontology != null)
+    //       .map(variable => ({
+    //         ...variable.observation_ontology,
+    //         type: 'observation',
+    //       }));
+    const outputVariables = !state.cohort.input_variables
+      ? []
+      : state.cohort.input_variables
+          .filter(variable => variable.observation_ontology != null)
+          .map(variable => {
+            const dimLabel = variable.dimension_label;
+            let dimension = '';
+            let label = '';
+            if (dimLabel === 'left_y_axis') {
+              dimension = 'firstVisit';
+              label = 'First Visit';
+            } else if (dimLabel === 'right_y_axis') {
+              dimension = 'lastVisit';
+              label = 'Last Visit';
+            } else if (dimLabel === 'roc') {
+              dimension = 'roc';
+              label = 'Rate of Change';
+            } else if (dimLabel === 'change') {
+              dimension = 'change';
+              label = 'Change';
+            }
+
+            return {
+              parentID: variable.observation_ontology_id,
+              parentLabel: variable.observation_ontology.label,
+              ...variable.observation_ontology,
+              id: `${dimension}-${variable.observation_ontology_id}`,
+              label,
+              type: 'observation',
+            };
+          });
+    return outputVariables;
+  },
+  [getters.FIND_COHORT_OBSERVATION_OUTPUT_VARIABLES]: state => {
+    const outputVariables = !state.cohort.output_variables
+      ? []
+      : state.cohort.output_variables
+          .filter(variable => variable.observation_ontology != null)
+          .map(variable => {
+            const dimLabel = variable.dimension_label;
+            let dimension = '';
+            let label = '';
+            if (dimLabel === 'left_y_axis') {
+              dimension = 'firstVisit';
+              label = 'First Visit';
+            } else if (dimLabel === 'right_y_axis') {
+              dimension = 'lastVisit';
+              label = 'Last Visit';
+            } else if (dimLabel === 'roc') {
+              dimension = 'roc';
+              label = 'Rate of Change';
+            } else if (dimLabel === 'change') {
+              dimension = 'change';
+              label = 'Change';
+            }
+
+            return {
+              parentID: variable.observation_ontology_id,
+              parentLabel: variable.observation_ontology.label,
+              ...variable.observation_ontology,
+              id: `${dimension}-${variable.observation_ontology_id}`,
+              label,
+              type: 'observation',
+            };
+          });
+
+    const ALL_DIMENSIONS_SELECTED = 4;
+    const outcomeMeasures = nest()
+      .key(d => d.parentID)
+      .entries(outputVariables)
+      .filter(d => d.values.length === ALL_DIMENSIONS_SELECTED)
+      .map(d => {
+        const id = Number(d.key);
+        const observation = state.cohort.output_variables.find(
+          d => d.observation_ontology_id === id
+        );
+        return {
+          ...observation.observation_ontology,
+          children: d.values,
+        };
+      });
+
+    // If a user selected a study with all of its dimensions,
+    // we want add just the one study and remove dimensions
+    // so we can draw a parallel coordinates plot
+    const parentIDs = outcomeMeasures.map(m => m.id);
+    let dimensionsNotInParentIDs = outputVariables.filter(
+      obs => !parentIDs.includes(obs.parentID)
+    );
+
+    return [...outcomeMeasures, ...dimensionsNotInParentIDs];
   },
 };
