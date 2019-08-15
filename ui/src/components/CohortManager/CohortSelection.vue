1f0035c921f29346888b6dbe10d91562e62d2351
Brian Gottfried <bgottf@gmail.com>
2019-08-15 16:03:54 -0400

    Finish cohort selection component

--- ui/src/components/CohortManager/CohortSelection.vue
+++ ui/src/components/CohortManager/CohortSelection.vue
@@ -1,17 +1,55 @@
 <template>
   <v-select
+    v-model="selectedCohort"
     flat
     dark
-    background-color=""
-    color="white"
-    :items="[]"
+    :items="cohorts"
+    item-text="label"
+    item-value="id"
+    return-object
     label="Cohorts"
     solo-inverted
   ></v-select>
 </template>
 
 <script>
-export default {};
+import { mapActions, mapState } from 'vuex';
+import { actions, state } from '@/store/modules/cohortManager/types';
+
+export default {
+  data() {
+    return {
+      selectedCohort: { id: null },
+    };
+  },
+  computed: {
+    ...mapState('cohortManager', {
+      _cohorts: state.COHORTS,
+      cohort: state.COHORT,
+    }),
+    cohorts() {
+      // include a default select that serves as the new cohort option
+      return [{ id: null, label: 'New Cohort' }, ...this._cohorts];
+    },
+  },
+  watch: {
+    selectedCohort(newCohort) {
+      this.setCohort(newCohort);
+    },
+    cohort() {
+      this.selectedCohort = this.cohort;
+    },
+  },
+  created() {
+    this.fetchCohorts();
+  },
+  methods: {
+    ...mapActions('cohortManager', {
+      fetchCohorts: actions.FETCH_COHORTS,
+      setCohort: actions.SET_COHORT,
+    }),
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
