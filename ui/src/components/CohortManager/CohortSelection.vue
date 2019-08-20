0efac37e04d85c0dac088ba3728a1c0323d558b3
Jonathan Crabtree <jonathancrabtree@gmail.com>
2019-08-20 15:48:58 -0400

    Cohorts are Collection-specific. Fixes #145.

--- ui/src/components/CohortManager/CohortSelection.vue
+++ ui/src/components/CohortManager/CohortSelection.vue
@@ -26,10 +26,22 @@ export default {
     ...mapState('cohortManager', {
       _cohorts: state.COHORTS,
       cohort: state.COHORT,
+      collection: state.COLLECTION
     }),
     cohorts() {
       // include a default select that serves as the new cohort option
-      return [{ id: null, label: 'New Cohort' }, ...this._cohorts];
+      var new_ch =  [{ id: null, label: 'New Cohort' }]
+      var ch = this._cohorts;
+      var cid = this.collection.id;
+
+      // cohorts are collection-specific
+      ch.forEach(function(e) {
+        if (e.collection_id === cid) {
+           new_ch.unshift(e);
+        }
+      });
+
+      return new_ch;
     },
   },
   watch: {
