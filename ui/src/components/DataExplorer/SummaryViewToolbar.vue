d7b524372fd082c1e82c484cd73d769fd1bca9d9
Jonathan Crabtree <jonathancrabtree@gmail.com>
2019-11-04 16:15:14 -0500

    Add tooltip to Data Explorer button to add outcome vars.

--- ui/src/components/DataExplorer/SummaryViewToolbar.vue
+++ ui/src/components/DataExplorer/SummaryViewToolbar.vue
@@ -1,25 +1,18 @@
 <template>
   <section>
+  <v-sheet color="white" height="100%" class="rounded-lg shadow">
+    <v-layout column fill-height class="ma-1">
+
     <v-toolbar card dense flat color="white rounded-lg">
-      <!-- <v-toolbar-items> <input-variables-dialog /> </v-toolbar-items> -->
       <span class="title primary--text">Summary View</span>
       <v-divider vertical class="ml-4"></v-divider>
       <v-toolbar-items> <outcome-variables-dialog /> </v-toolbar-items>
-      <v-spacer />
-      <!-- <v-toolbar-items>
-      </v-toolbar-items> -->
-      <!-- <v-spacer></v-spacer>
-      <v-select
-        :items="['Cohort']"
-        menu-props="auto"
-        label="Select"
-        hide-details
-        append-icon="format_color_fill"
-        color="primary"
-      ></v-select> -->
-      <!-- <v-spacer></v-spacer> -->
+
     </v-toolbar>
     <v-divider></v-divider>
+    </v-layout>
+  </v-sheet>
+
   </section>
 </template>
 
