a63e355521a7c22ab9ebbc300b9584c9d7e9494f
Jonathan Crabtree <jonathancrabtree@gmail.com>
2020-01-10 16:22:15 -0500

    Try yellow instead of grey for tracker highlights.

--- ui/src/components/common/AnalysisTracker.vue
+++ ui/src/components/common/AnalysisTracker.vue
@@ -181,7 +181,7 @@ export default {
     currentStepStyle: {
       type: String,
       default:
-        'background: rgb(100,100,200,0.2); border-left: 1px dotted black; border-right: 1px dotted black; border-top: 1px dotted black;',
+        'background: rgb(212,197,71,0.2); border-left: 1px dotted black; border-right: 1px dotted black; border-top: 1px dotted black;',
       required: false,
     },
   },
