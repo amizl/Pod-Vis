af441f2c0f420c8a69be93d2ba5650c2b74ba467
Jonathan Crabtree <jonathancrabtree@gmail.com>
2020-02-06 10:20:27 -0500

    Add missing view for #275.

---
+++ ui/src/views/StudyDataset.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container fluid class="ma-0 pa-2">
+    <v-toolbar app class="primary">
+      <v-toolbar-title class="white--text"
+        >Use Saved Study Dataset</v-toolbar-title
+      >
+    </v-toolbar>
+
+    <v-sheet color="white" class="scroll rounded-lg shadow mt-2">
+      <v-layout fill-height>
+        <v-divider></v-divider>
+
+        <v-flex xs12 class="pa-0 ma-0">
+          <collection-table
+            class="pb-1"
+            show-collections="all"
+          ></collection-table>
+        </v-flex>
+      </v-layout>
+    </v-sheet>
+  </v-container>
+</template>
+
+<script>
+import CollectionTable from '@/components/dashboard/CollectionTable.vue';
+
+export default {
+  components: {
+    CollectionTable,
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style scoped></style>
