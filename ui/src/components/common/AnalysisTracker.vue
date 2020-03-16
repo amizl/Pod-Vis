6811cde95a6f7cb85aba036cf78f8f8164ff6382
Jonathan Crabtree <jonathancrabtree@gmail.com>
2020-03-16 14:51:49 -0400

    Add lint fixes.

--- ui/src/components/common/AnalysisTracker.vue
+++ ui/src/components/common/AnalysisTracker.vue
@@ -170,24 +170,19 @@
             <span v-if="collection_cohorts.length === 1">cohort has</span>
             <span v-else>cohorts have</span> been defined. To proceed to the
             Summary Matrix a minimum of 3 distinct cohorts must be created and
-            saved. Click
-            <v-btn @click="createSimilar()">Create Similar Cohort</v-btn> to
-            create another cohort based on the most-recently-saved cohort, or
-            <v-btn @click="goto3p1()">Create New Cohort</v-btn> to create an
-            entirely new cohort from scratch. "Create Similar Cohort" can be
-            useful, for example, if you'd like to keep the same set of predictor
-            and output variables across several cohorts.
+            saved.<br clear='both'>
+	    Click on <v-btn @click="createSimilar()">Create Similar Cohort</v-btn> to
+            create another cohort based on the last one, or <v-btn @click="goto3p1()">Create New Cohort</v-btn>
+	    to create a new cohort from scratch.
           </div>
           <div v-else>
-            {{ collection_cohorts.length }} cohorts have been created and saved.
-            If you wish to create another cohort, click on
-            <v-btn @click="createSimilar()">Create Similar Cohort</v-btn> to
-            create another cohort based on the most-recently-saved cohort, or
-            <v-btn @click="goto3p1()">Create New Cohort</v-btn> to create an
-            entirely new cohort from scratch. Otherwise, click
-            <v-btn @click="gotoSummaryMatrix()">Continue</v-btn> to proceed to
+            {{ collection_cohorts.length }} cohorts have been created and saved.<br clear='both'>
+	    Click on <v-btn @click="createSimilar()">Create Similar Cohort</v-btn> to
+            create another cohort based on the last one, <v-btn @click="goto3p1()">Create New Cohort</v-btn>
+	    to create a new cohort from scratch,
+            or <v-btn @click="gotoSummaryMatrix()">Continue</v-btn> to proceed to
             the Summary Matrix.
-          </div>
+           </div>
         </v-stepper-content>
       </v-stepper-items>
     </v-stepper>
