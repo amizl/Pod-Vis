de72556cfd3b8e017f12f241d78976960acb064f
Jonathan Crabtree <jonathancrabtree@gmail.com>
2020-03-19 16:48:32 -0400

    Add data_category and value_type.

--- api/app/models/subject_ontology.py
+++ api/app/models/subject_ontology.py
@@ -1,4 +1,16 @@
 from . import db
+import enum
+
+class DataCategory(enum.Enum):
+    Categorical = "Categorical"
+    Ordinal = "Ordinal"
+    Continuous = "Continuous"
+
+class ValueType(enum.Enum):
+    int = "int"
+    decimal = "decimal"
+    char = "char"
+    date = "date"
 
 class SubjectOntology(db.Model):
     __tablename__ = "subject_ontology"
@@ -6,12 +18,17 @@ class SubjectOntology(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     parent_id = db.Column(db.Integer, db.ForeignKey("subject_ontology.id"))
     label = db.Column(db.VARCHAR, nullable=False)
+    value_type = db.Column(db.Enum(ValueType))
+    data_category = db.Column(db.Enum(DataCategory))
 
     parent = db.relationship("SubjectOntology", remote_side=[id])
 
-    def __init__(self,  parent_id, label):
+    def __init__(self,  parent_id, label, value_type, data_category):
         self.parent_id = parent_id
-        self.label= label
+        self.label = label
+        self.value_type = value_type
+        self.data_category = data_category
+
 
     @classmethod
     def get_all_subject_ontology(cls):
@@ -50,6 +67,11 @@ class SubjectOntology(db.Model):
           parent_id=self.parent_id,
           label=self.label
         )
+
+        if self.value_type is not None:
+            ontology['value_type'] = self.value_type.value
+        if self.data_category is not None:
+            ontology['data_category'] = self.data_category.value
 
         if include_parent and self.parent:
             ontology['parent'] = self.parent.to_dict()
