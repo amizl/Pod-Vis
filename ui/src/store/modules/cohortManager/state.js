27afbdf4876b13950695844a32cbeee3854d26c9
Jonathan Crabtree <jonathancrabtree@gmail.com>
2020-03-31 15:19:29 -0400

    Set default p-value threshold to 0.001 Fixes #356.

--- ui/src/store/modules/cohortManager/state.js
+++ ui/src/store/modules/cohortManager/state.js
@@ -12,7 +12,7 @@ export default {
   [state.CROSS_FILTER]: null,
   [state.DIMENSIONS]: {},
   [state.PVALS]: [],
-  [state.PVAL_THRESHOLD]: 0.0001,
+  [state.PVAL_THRESHOLD]: 0.001,
   [state.PVALS_REQUEST]: [],
   [state.HIGHLIGHTED_SUBSET]: 'cohort',
   [state.QUERIES]: {},
