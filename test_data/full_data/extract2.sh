7573f88289244a69bc0b935512c609b4cc477e79
Jonathan Crabtree <jonathancrabtree@gmail.com>
2020-04-13 09:49:55 -0400

    Add full_data from fall-2019 branch.

---
+++ test_data/full_data/extract2.sh
@@ -0,0 +1,5 @@
+for i in winnow_demographics_long.csv winnow_obs_long.csv winnow_obs_summary.csv
+    do echo $i
+    cat $i | awk 'BEGIN {FS=","}; NR==1 {print $0}; {if($1 == "245" || $1 == "432") {print $0}}' > ../${i}
+done
+
