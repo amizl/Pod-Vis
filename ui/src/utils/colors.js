e515db8b102e7a9a240219ec7b49a88092bd7c70
Jonathan Crabtree <jonathancrabtree@gmail.com>
2020-04-20 09:53:03 -0400

    Switch to silver/blue Cohort Manager color scheme.

--- ui/src/utils/colors.js
+++ ui/src/utils/colors.js
@@ -1,21 +1,21 @@
-
 // original - blue, yellow, green
+/*
 export let colors = {
-  'population': '#F8D580',
+  population: '#F8D580',
   'cohort-circle-fill': 'blue',
   'cohort-circle-fill-opacity': '0.8',
   'cohort-circle-stroke': '#2bdb60',
   'cohort-circle-stroke-width': '2',
 
-  'cohort': '#3F51B5',
-  'population-circle-fill': "#F88123",
+  cohort: '#3F51B5',
+  'population-circle-fill': '#F88123',
   'population-circle-fill-opacity': '0.8',
-  'population-circle-stroke': "#f8d537",
+  'population-circle-stroke': '#f8d537',
   'population-circle-stroke-width': '2',
 
-  'nonCohort': '#3FB551',
+  nonCohort: '#3FB551',
 };
-
+*/
 // silver/grey
 /*
 export let colors = {
@@ -32,3 +32,43 @@ export let colors = {
   'nonCohort': '#3FB551',
 };
 */
+
+// silver/grey/"light blue"
+/*
+export let colors = {
+  'population': '#8C99A5',
+  'cohort-circle-fill': 'blue',
+  'cohort-circle-fill-opacity': '0.8',
+  'cohort-circle-stroke': '#2bdb60',
+  'cohort-circle-stroke-width': '2',
+  'cohort': '#3F51B5',
+  'population-circle-fill': "#F88123",
+  'population-circle-fill-opacity': '0.8',
+  'population-circle-stroke': "#f8d537",
+  'population-circle-stroke-width': '2',
+  'nonCohort': '#3FB551',
+};
+*/
+// grey/light blue
+export let colors = {
+    'cohort': '#3F51B5',
+    'cohort-circle-fill': '#1B33B5',
+    'cohort-circle-fill-opacity': '1',
+    'cohort-circle-stroke': '#7680B5',
+    'cohort-circle-stroke-width': '2',
+    
+    'population': '#B8D6FF', 
+    'population-circle-fill': '#85B9FF',
+    'population-circle-fill-opacity': '1',
+    'population-circle-stroke': "#FFFFFF",
+    'population-circle-stroke-width': '2',
+
+    /*
+    'population-circle-fill': "#F88123",
+  'population-circle-fill-opacity': '0.8',
+  'population-circle-stroke': "#f8d537",
+  'population-circle-stroke-width': '2',
+*/
+    'nonCohort': '#3FB551',
+};
+
