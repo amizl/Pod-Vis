e2b4f8a1913e2fc7c327d3abc5760d8cdfc538f6
Jonathan Crabtree <jonathancrabtree@gmail.com>
2020-07-16 09:22:00 -0400

    Rename HealthSummary as per #323.

--- bin/patch-ppmi-field-mapping.sh
+++ bin/patch-ppmi-field-mapping.sh
@@ -4,6 +4,6 @@
 
 echo "patching $1"
 
-perl -pi.bak -e 's/Number sequencing/Number Sequencing/g; s/REM Sleep Disorder/REM Sleep Behavior Disorder Questionnaire/g; s/Symbol Digit Modality/Symbol Digit Modalities/g; s/Geriatric Depression State Score/Geriatric Depression Scale Score/g; s/Geriatric Depression State/Geriatric Depression Scale State/g;' $1
+perl -pi.bak -e 's/Number sequencing/Number Sequencing/g; s/REM Sleep Disorder/REM Sleep Behavior Disorder Questionnaire/g; s/Symbol Digit Modality/Symbol Digit Modalities/g; s/Geriatric Depression State Score/Geriatric Depression Scale Score/g; s/Geriatric Depression State/Geriatric Depression Scale State/g;s/HealthSummary/Disease Features/g;' $1
 
 
