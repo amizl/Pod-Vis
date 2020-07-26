9c9cb51cadb9fc8ee1ade08e293e9aa698e34e4e
Jonathan Crabtree <jonathancrabtree@gmail.com>
2020-07-26 10:22:40 -0400

    Remove v-layout, simplify document structure.

--- ui/src/components/layout/Sidebar.vue
+++ ui/src/components/layout/Sidebar.vue
@@ -5,88 +5,100 @@
     app
     permanent
     fixed
-    class=""
   >
-    <v-layout column fill-height>
-      <!-- POD-Vis Logo -->
-      <v-list three-line class="pt-3">
-        <v-list-item>
-          <div v-if="showExpanded()">
-            <img
-              width="100%"
-              src="/images/POD-Vis_tag2.jpg"
-              alt="POD-Vis: Probing Outcomes Data with Visual analytics"
+    <!-- POD-Vis Logo -->
+    <v-list class="pt-3">
+      <v-list-item>
+        <div v-if="showExpanded()">
+          <img
+            width="100%"
+            src="/images/POD-Vis_tag2.jpg"
+            alt="POD-Vis: Probing Outcomes Data with Visual analytics"
             />
-          </div>
-          <div v-else>
-            <img
-              width="100%"
-              src="/images/POD-Vis.jpg"
-              alt="POD-Vis: Probing Outcomes Data with Visual analytics"
+        </div>
+        <div v-else>
+          <img
+            width="100%"
+            src="/images/POD-Vis.jpg"
+            alt="POD-Vis: Probing Outcomes Data with Visual analytics"
             />
-          </div>
-        </v-list-item>
-      </v-list>
-
+        </div>
+      </v-list-item>
+      
       <!-- Application links -->
-      <v-list class="pt-0">
-        <v-list-item
-          v-for="item in menuItems"
-          :key="item.title"
-          :to="item.path"
+      <v-list-item
+        v-for="item in menuItems"
+        :key="item.title"
+        :to="item.path"
+	justify="center"
         >
-          <!-- icon without tooltip -->
-          <v-list-item-icon v-if="showExpanded()">
-            <v-icon color="primary" large>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
+        <!-- icon without tooltip -->
+        <v-list-item-icon v-if="showExpanded()">
+          <v-icon color="primary" large>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+	
+        <!-- icon with tooltip -->
+        <v-tooltip v-else color="primary" right>
+          <template v-slot:activator="{ on: tooltip }">
+            <v-list-item-action v-on="{ ...tooltip }">
+              <v-icon color="primary" large>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+          </template>
+          <span>{{ item.name }}</span>
+        </v-tooltip>
+	
+        <v-list-item-content class="primary--text">
+          <span class="title-1">{{ item.name }}</span>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
 
-          <!-- icon with tooltip -->
-          <v-tooltip v-else color="primary" right>
-            <template v-slot:activator="{ on: tooltip }">
-              <v-list-item-action v-on="{ ...tooltip }">
-                <v-icon color="primary" large>{{ item.icon }}</v-icon>
-              </v-list-item-action>
-            </template>
-            <span>{{ item.name }}</span>
-          </v-tooltip>
+    <!-- SIGN OUT DIALOG -->
+    <v-dialog v-model="signOutDialog" width="500" persistent>
+      <v-card>
+        <v-card-title color="white" primary-title>
+          <v-icon color="primary darken-3">warning</v-icon>
+          <span class="primary--text text--darken-3 title pl-2">Sign Out</span>
+        </v-card-title>
+        <v-card-text class="primary primary--text text--lighten-5 pt-4"
+		     >Are you sure you'd like to sign out?</v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text color="primary" @click="signOutDialog = false"
+            ><v-icon left>close</v-icon>Cancel</v-btn
+          >
+          <v-btn color="primary" @click="signUserOut">Sign Out</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
-          <v-list-item-content class="primary--text">
-            <span class="title-1">{{ item.name }}</span>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+    <template v-slot:append>
+      <v-list>
 
-      <v-spacer></v-spacer>
-
-      <!-- Minimize/expand, sign out -->
-      <v-list one-line>
-        <!-- Force expanded sidebar on homepage -->
-        <v-list-item v-if="this.$route.name === 'homepage'"> </v-list-item>
-
-        <!-- minimize -->
-        <v-list-item v-else @click="expand = !expand">
+        <!-- minimize/expand -->
+        <v-list-item v-if="$route.name !== 'homepage'" @click="expand = !expand">
           <v-list-item-action v-if="showExpanded()">
             <v-icon color="primary" x-large>chevron_left</v-icon>
           </v-list-item-action>
-
-          <!-- expand -->
-          <v-tooltip v-else color="primary" right>
-            <template v-slot:activator="{ on: tooltip }">
-              <v-list-item-action v-on="{ ...tooltip }">
-                <v-icon color="primary" x-large>chevron_right</v-icon>
-              </v-list-item-action>
-            </template>
-            <span>Expand menu</span>
-          </v-tooltip>
-
-          <!-- title -->
-          <v-list-item-content v-if="showExpanded()">
-            <v-list-item-title>
+	  
+          <v-list-item-action v-else>
+	    <v-tooltip color="primary" right>
+              <template v-slot:activator="{ on: tooltip }">
+		<v-icon v-on="{ ...tooltip}" color="primary" x-large>chevron_right</v-icon>
+	      </template>
+	      <span>Expand menu</span>
+	    </v-tooltip>
+          </v-list-item-action>
+	  
+	  <v-list-item-content>
+	    <v-list-item-title v-if="showExpanded()">
               <span class="primary--text">Minimize menu</span>
             </v-list-item-title>
+	    
           </v-list-item-content>
         </v-list-item>
-
+	
         <!-- sign out -->
         <v-list-item @click="signOutDialog = true">
           <v-list-item-action v-if="showExpanded()">
@@ -107,28 +119,8 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
-    </v-layout>
+    </template>
 
-    <!-- SIGN OUT DIALOG -->
-    <v-dialog v-model="signOutDialog" width="500" persistent>
-      <v-card>
-        <v-card-title color="white" primary-title>
-          <v-icon color="primary darken-3">warning</v-icon>
-          <span class="primary--text text--darken-3 title pl-2">Sign Out</span>
-        </v-card-title>
-        <v-card-text class="primary primary--text text--lighten-5 pt-4"
-          >Are you sure you'd like to sign out?</v-card-text
-        >
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn text color="primary" @click="signOutDialog = false"
-            ><v-icon left>close</v-icon>Cancel</v-btn
-          >
-          <v-btn color="primary" @click="signUserOut">Sign Out</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </v-navigation-drawer>
 </template>
 
